ea8a1f6d5984179caf4691e7ccc3a98c502a2675
raparthiviswaxen-ai <raparthiviswaxen@gmail.com>
2026-06-01 06:01:23 -0400

    scopes and closure

---
+++ scopes/app.js
@@ -0,0 +1,43 @@
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+
+if (true){
+  var d = 100;
+  let e = 200;
+ const f = 300;
+
+}
+
+sam()
+
+function sam(){
+    var g = 1000;
+    let h = 2000;
+    const i = 3000;
+}
+
+
+// need to focus more rather that practicing once need to spend more time.
+// most important topic :
+
+function bob(){
+    var a = 200;
+    let b = 300;
+    const c = 400;
+    function don(){
+        console.log(a+b);
+    }
+    return don
+}
+
+
+let out = bob()
+
+out();
